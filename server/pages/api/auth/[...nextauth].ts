07f7ea7ebd089552d7fffb2017cf1a7a4663ccc2
Duncan Band <duncan@littlehobbyshop.co.uk>
2023-02-02 22:51:47 +0000

    fix authentication

--- server/pages/api/auth/[...nextauth].ts
+++ server/pages/api/auth/[...nextauth].ts
@@ -1,14 +1,19 @@
-import NextAuth from "next-auth"
-import DiscordProvider from "next-auth/providers/discord"
-import { PrismaAdapter } from "@next-auth/prisma-adapter"
-import prisma from "../../../lib/prisma"
+import { NextApiHandler } from 'next';
+import NextAuth from 'next-auth';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import DiscordProvider from 'next-auth/providers/discord';
+import prisma from '../../../lib/prisma';
 
-export default NextAuth({
-  adapter: PrismaAdapter(prisma),
+const authHandler: NextApiHandler = (req, res) => NextAuth(req, res, options);
+export default authHandler;
+
+const options = {
   providers: [
     DiscordProvider({
-        clientId: process.env.DISCORD_CLIENT_ID,
-        clientSecret: process.env.DISCORD_CLIENT_SECRET
-      })
+      clientId: process.env.DISCORD_CLIENT_ID,
+      clientSecret: process.env.DISCORD_CLIENT_SECRET,
+    }),
   ],
-})
+  adapter: PrismaAdapter(prisma),
+  secret: process.env.SECRET,
+};
