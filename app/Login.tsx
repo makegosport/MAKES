0a0ddc0339eb9dfb3432026505eff5c852326503
Duncan Band <band.duncan@gmail.com>
2023-04-15 12:46:50 +0100

    feat: github auth

---
+++ app/Login.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+import { useSupabase } from "./supabase-provider";
+import { User } from "@supabase/supabase-js";
+
+const Login = () => {
+  const [user, setUser] = useState<undefined | User>(undefined);
+  const { supabase } = useSupabase();
+
+  useEffect(() => {
+    supabase.auth.onAuthStateChange((event, session) => {
+      setUser(session?.user);
+    });
+  }, [supabase]);
+
+  const signIn = () => {
+    supabase.auth.signInWithOAuth({
+      provider: "discord",
+    });
+  };
+
+  const signOut = () => {
+    supabase.auth.signOut();
+  };
+
+  return (
+    <div>
+      {user ? (
+        <div>{user.email}</div>
+      ) : (
+        <button onClick={signIn}>Sign In</button>
+      )}
+      <button onClick={signOut}>Sign Out</button>
+    </div>
+  );
+};
+
+export default Login;
